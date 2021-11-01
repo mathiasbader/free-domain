919de0b4601d2ca93228b50690fda1982fb95d08
Mathias Bader <mail@mathiasbader.de>
2021-11-01 14:04:54 +0100

    Add two favicon files

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <meta name="viewport" content="width=device-width, inital-scale=1.0">
 
     <link rel="apple-touch-icon" sizes="256x256" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 
     <title><?php echo TITLE; ?></title>
